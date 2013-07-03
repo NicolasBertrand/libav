447d0e2ad97ac4b1455e6393b8638866dc2673d3
Nicolas Bertrand <nicoinattendu@gmail.com>
2013-07-03 15:13:24 +0200

    jpeg2000: Dynamically allocate code-block data.

--- libavcodec/jpeg2000.c
+++ libavcodec/jpeg2000.c
@@ -509,6 +509,13 @@ void ff_jpeg2000_cleanup(Jpeg2000Component *comp, Jpeg2000CodingStyle *codsty)
                     continue;
 
                 prec = band->prec + precno;
+                int nb_code_blocks = prec->nb_codeblocks_height * prec->nb_codeblocks_width;
+                int cblkno;
+                for (cblkno = 0; cblkno < nb_code_blocks; cblkno++) {
+                    Jpeg2000Cblk *cblk = prec->cblk + cblkno;
+                    av_freep(&cblk->data);
+                }
+
                 av_freep(&prec->zerobits);
                 av_freep(&prec->cblkincl);
                 av_freep(&prec->cblk);
